e86f7d030acb973fbd2576ca09a1cc0fc8105edd
Ryo Miyake <ryo.studiom@gmail.com>
2017-05-04 10:17:17 +0900

    Add python setup files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='nekoya-press',
+    packages=['blog'],
+    version='0.2.0',
+    author='nekoya',
+    author_email='ryo.studiom@gmail.com',
+    description=""
+                "",
+    long_description=open('README.md').read(),
+    url='',
+    license='MIT',
+    tests_require=[],
+    test_suite='',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
